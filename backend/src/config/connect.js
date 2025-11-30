d2d8ac3ee06c6494644b4bae9bd58a1e1fd27dda
KaoM3 <152694740+KaoM3@users.noreply.github.com>
2025-12-01 07:03:39 +0800

    feat: add enableKeepAlive to SQL connection

--- backend/src/config/connect.js
+++ backend/src/config/connect.js
@@ -57,13 +57,18 @@ export const initDB = async () => {
 
   console.log('Initializing database connection pool...');
 
-  // Connect to pool 1
+  // Connect to pool
   pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true, // Pings DB to keep connection open
+    keepAliveInitialDelay: 0,
   });
 
   try {
