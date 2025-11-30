f87500a63dce57daeeb3b3a84cd888094dc85cbd
KaoM3 <152694740+KaoM3@users.noreply.github.com>
2025-12-01 05:47:25 +0800

    fix: receivePrepare now passes timestamp to handlePrepare

--- backend/src/controllers/internal.controller.js
+++ backend/src/controllers/internal.controller.js
@@ -6,9 +6,15 @@ export const clientRequest = async (req, res) => {
 
 export async function receivePrepare(req, res) {
   try {
-    await internalService.handlePrepare(req.body.transactionId, req.body.data);
+    const {transactionId, timestamp, data } = req.body
+    if (!transactionId || !timestamp || !data) {
+      console.error("Missing 2PC headers:", req.body);
+      return res.status(400).json({ vote: 'NO', error: "Missing transaction headers" });
+    }
+    await internalService.handlePrepare(transactionId, timestamp, data);
     res.status(200).json({ vote: 'YES' });
   } catch (error) {
+    console.error("RECEIVE PREPARE ERROR: ", error)
     res.status(500).json({ vote: 'NO', error: error.message });
   }
 }
@@ -18,6 +24,7 @@ export async function receiveCommit(req, res) {
     await internalService.handleCommit(req.body.transactionId);
     res.status(200).json({ status: 'COMMITTED' });
   } catch (error) {
+    console.error("RECEIVE COMMIT ERROR: ", error)
     res.status(500).json({ status: 'FAILED TO COMMIT', error: error.message });
   }
 }
@@ -27,6 +34,7 @@ export async function receiveAbort(req, res) {
     await internalService.handleAbort(req.body.transactionId);
     res.status(200).json({ status: 'ABORTED' });
   } catch (error) {
+    console.error("RECEIVE ABORT ERROR: ", error)
     res.status(500).json({ status: 'FAILED TO ABORT', error: error.message });
   }
 }
