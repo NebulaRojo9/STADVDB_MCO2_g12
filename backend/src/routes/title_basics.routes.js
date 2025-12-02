08024e989648290b4bf3c34ccef88b8e56ddd1f7
KaoM3 <152694740+KaoM3@users.noreply.github.com>
2025-12-02 14:20:15 +0800

    fix: add formatDelay middleware to title basic crud routes

--- backend/src/routes/title_basics.routes.js
+++ backend/src/routes/title_basics.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as titleBasicsCrudController from '../controllers/title_basics_crud.controller.js'
 import { requireStartYear } from '../middleware/fragment_validator.js';
+import { formatDelay } from '../middleware/delay_validator.js';
 
 const router = Router();
 
@@ -9,9 +10,9 @@ router.delete('/resetDatabases', titleBasicsCrudController.resetDatabases);
 
 router.get('/readAll', titleBasicsCrudController.readTitleAll);
 router.get('/readAllFromNode', titleBasicsCrudController.readTitleFromNode);
-router.post('/create', requireStartYear, titleBasicsCrudController.createTitle);
-router.get('/read/:id', requireStartYear, titleBasicsCrudController.readTitle);
-router.put('/update/:id', requireStartYear, titleBasicsCrudController.updateTitle);
-router.delete('/delete/:id', requireStartYear, titleBasicsCrudController.deleteTitle);
+router.post('/create', requireStartYear, formatDelay, titleBasicsCrudController.createTitle);
+router.get('/read/:id', requireStartYear, formatDelay, titleBasicsCrudController.readTitle);
+router.put('/update/:id', requireStartYear, formatDelay, titleBasicsCrudController.updateTitle);
+router.delete('/delete/:id', requireStartYear, formatDelay, titleBasicsCrudController.deleteTitle);
 
 export default router;
